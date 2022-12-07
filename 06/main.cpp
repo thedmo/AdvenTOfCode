53e169332070a89dd896ead8762b5dd0d603698e
Benjamin Hauser <benjaminehauser@gmail.com>
2022-12-07 21:01:52 +0100

    Solution part 2

--- 06/main.cpp
+++ 06/main.cpp
@@ -1,6 +1,9 @@
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
+
+const int SEQUENCE_LENGTH = 14;
 
 int main() {
   std::ifstream input("../../input.txt");
@@ -9,28 +12,34 @@ int main() {
   std::getline(input, line);
 
   std::string sequence;
-  int index;
+  int index = 0;
+  std::string current_sequence;
+  std::map<char, int> sequence_map;
+  bool all_chars_unique = true;
 
   for (size_t i = 0; i < line.size(); i++) {
-    if (line.at(i) == line.at(i + 1)) {
-      continue;
-    } else {
-      if (line.at(i + 1) == line.at(i + 2) || line.at(i) == line.at(i + 2)) {
-        continue;
+    // initialize
+    current_sequence = line.substr(i, SEQUENCE_LENGTH);
+    sequence_map.clear();
+    all_chars_unique = true;
+
+    for (size_t j = 0; j < current_sequence.size(); j++) {
+      if (sequence_map.find(current_sequence.at(j)) != sequence_map.end()) {
+        all_chars_unique = false;
+        break;
       } else {
-        if (line.at(i + 2) == line.at(i + 3) || line.at(i + 1) == line.at(i + 3) || line.at(i) == line.at(i + 3)) {
-          continue;
-        } else {
-          sequence += line.at(i);
-          sequence += line.at(i + 1);
-          sequence += line.at(i + 2);
-          sequence += line.at(i + 3);
-          index = i + 4;
-          break;
-        }
+        sequence_map.insert(std::pair<char, int>(current_sequence.at(j), 1));
       }
     }
-  }
 
+    std::cout << current_sequence << std::endl;
+    if (!all_chars_unique) {
+      continue;
+    } else {
+      index = i + SEQUENCE_LENGTH;
+      sequence = current_sequence;
+      break;
+    }
+  }
   std::cout << "Sequence is: " << sequence << " at index: " << index << std::endl;
 }
